c038990db96965bfe91c219cc103f3c124fc7950
Segrelove <mlesegret@gmail.com>
2018-11-29 10:56:08 +0100

    kind of the same commit as the last one, but I commit this version so I can have a working backup before I install Vuetify, whice is the package that basically destroyed my previous work...

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -8,6 +8,7 @@
     <input type="password" name="password"
     v-model="password"
     placeholder="password"/><br>
+    <div class="error" v-html="error"/>
     <button @click="register">Register</button>
   </div>
 </template>
@@ -17,21 +18,29 @@
 import AuthenticationService from '@/services/AuthenticationService'
 export default {
   data () {
-    return {email: '', password: ''
+    return {
+    email: '',
+    password: '',
+    error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data);
+      try {
+        const response = await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
-  }
+}
 </script>
 
 <style>
-
+.error {
+  color: red;
+}
 </style>
